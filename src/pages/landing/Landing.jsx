ca88b2cea678c69c6f84074efaf3cbba8cc1d8dc
JuanSBass <juan_santillan20@hotmail.com>
2023-01-08 22:38:23 -0600

    form desing

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,22 +1,27 @@
 import { Button } from "flowbite-react";
 import { HiLogin, HiTable } from "react-icons/hi";
+import { Link } from "react-router-dom";
 import styles from "./landing.module.css";
 
 const Landing = () => {
   return (
     <div className={styles.card}>
       <main>
-        <h1 className={styles.title}>¡Bienvenidx! <br /> <p>Selecciona una opción</p></h1>
+        <h1 className={styles.title}>
+          ¡Bienvenidx! <br /> <p>Selecciona una opción</p>
+        </h1>
+        <div>
+          <Link to="/register">
+            <Button color="dark">
+              Registrarse
+              <HiTable className="ml-2 h-5 w-5" />
+            </Button>
+          </Link>
+        </div>
         <div>
           <Button color="dark">
             <HiLogin className="mr-2 h-5 w-5" />
             Entrar
-          </Button>
-        </div>
-        <div>
-          <Button color="dark" >
-            Registrarse
-            <HiTable className="ml-2 h-5 w-5" />
           </Button>
         </div>
       </main>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,46 @@
+import { Button, Label, TextInput } from "flowbite-react";
+import { useState } from "react";
+import styles from "./register.module.css";
+import logo from "../../img/Aos logo.png";
 
 const Register = () => {
-  return (
-    <div>
-      <h1>Registrate</h1>
-    </div>
-  )
-}
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
 
-export default Register
+  return (
+    <div className={styles.card}>
+      <form className="flex flex-col gap-4">
+        <img src={logo} alt="logo" className={styles.logo} />
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="email1" value="Email" className={styles.title} />
+          </div>
+          <TextInput
+            id="email"
+            type="email"
+            placeholder="ejemplo@email.com"
+            required={true}
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label
+              htmlFor="password1"
+              value="Contraseña"
+              className={styles.title}
+            />
+          </div>
+          <TextInput id="password" type="password" required={true} />
+        </div>
+        <Button outline={true} gradientDuoTone="redToYellow" type="submite">
+          Registrarse
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
